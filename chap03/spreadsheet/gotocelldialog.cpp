d6218a6e54d92dab5a8c8bd79a39394ae97531a6
realfan <13817779880@163.com>
2017-09-29 10:34:46 +0800

    chap03 to Qt5
    chap03 to Qt5

--- chap03/spreadsheet/gotocelldialog.cpp
+++ chap03/spreadsheet/gotocelldialog.cpp
@@ -1,7 +1,5 @@
-#include <QtGui>
-
 #include "gotocelldialog.h"
-
+#include <QPushButton>
 GoToCellDialog::GoToCellDialog(QWidget *parent)
     : QDialog(parent)
 {
@@ -11,8 +9,8 @@ GoToCellDialog::GoToCellDialog(QWidget *parent)
     QRegExp regExp("[A-Za-z][1-9][0-9]{0,2}");
     lineEdit->setValidator(new QRegExpValidator(regExp, this));
 
-    connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
-    connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+    connect(buttonBox, &QDialogButtonBox::accepted, this, &GoToCellDialog::accept);
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &GoToCellDialog::reject);
 }
 
 void GoToCellDialog::on_lineEdit_textChanged()
